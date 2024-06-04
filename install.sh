ca0ea6b0fac398d51b879f889b915040b41419be
Burak Hamza <thedatcal@gmail.com>
2024-06-04 19:34:20 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,30 +1,60 @@
-# !/bin/bash
+#!/bin/bash
 
-# Ask for the administrator password upfront.
+# Function to check if a command exists
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
+}
+
+# Detect OS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    OS="mac"
+elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    OS="linux"
+else
+    echo "Unsupported OS: $OSTYPE"
+    exit 1
+fi
+
 sudo -v
-# Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
-
-
-# install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-
-# install via brew
-brew bundle --file=./Brewfile
 
 
 ################################### Symlink stuff ############################
 ################### (Backs up the previous versions if they exist) ###########
 # symlinks
 
-DOTFILES_ROOT="`pwd`"
+DOTFILES_ROOT="$(pwd)"
+
+backup_and_symlink() {
+    local file=$1
+    local target=$2
+
+    if [[ -e "$target" ]]; then
+        cp "$target" "${target}_bak"
+    fi
+
+    ln -sf "$DOTFILES_ROOT/$file" "$target"
+}
+
+# Install brew on macOS
+if [[ "$OS" == "mac" ]]; then
+    if ! command_exists brew; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+    
+    # Install via brew
+    brew bundle --file=./Brewfile
+
+    backup_and_symlink ".aliases" "$HOME/.aliases"
+    backup_and_symlink ".gitconfig" "$HOME/.gitconfig"
+    backup_and_symlink ".config/fish/config.fish" "$HOME/.config/fish/config.fish"
+    
+elif [[ "$OS" == "linux" ]]; then
+    echo "Please ensure you have the necessary packages installed via your package manager."
+    
+    backup_and_symlink ".aliases" "$HOME/.aliases"
+    backup_and_symlink ".gitconfig" "$HOME/.gitconfig"
+fi
 
 
-cp "$HOME/.alliases" "$HOME/.alliases_bak" 2>/dev/null
-cp "$HOME/.gitconfig" "$HOME/.gitconfig_bak" 2>/dev/null
-cp "$HOME/.config/fish/config.fish" "$HOME/.config/fish/config.fish_bak" 2>/dev/null
 
-
-ln -sf "$DOTFILES_ROOT/.alliases" "$HOME/.alliases"
-ln -sf "$DOTFILES_ROOT/.gitconfig" "$HOME/.gitconfig"
-ln -sf "$DOTFILES_ROOT/.config/fish/config.fish" "$HOME/.config/fish/config.fish"
